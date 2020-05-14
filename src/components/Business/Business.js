481c1cddb909e3515ca6f4a3376d18fda34314e7
Taimoor Neeshat <taimoor.neeshat@gmail.com>
2020-05-14 13:03:03 +0500

    Part 3 completed

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -9,7 +9,7 @@ class Business extends React.Component {
                     <img src={this.props.business.imageSrc} alt=''/>
                 </div>
                 <h2>{this.props.business.name}</h2>
-                <div class="Business-information">
+                <div className="Business-information">
                     <div className="Business-address">
                     <p>{this.props.business.address}</p>
                     <p>{this.props.business.city}</p>
